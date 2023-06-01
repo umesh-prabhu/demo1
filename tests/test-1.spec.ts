f11c8284132214f5283a76387751c72b2ea9de4a
umesh <umesh.prabhu@aldi.us>
2023-05-31 22:18:20 -0500

    aa

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,9 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  //await page.goto('https://playwright.dev/');
-  //await page.getByRole('link', { name: 'Get started' }).click();
-  await page.goto('https://help.aldi.us/s/gift-card-balance')
+   await page.goto('https://help.aldi.us/s/gift-card-balance')
   page.waitForLoadState('domcontentloaded')
   await page.screenshot({ path: 'gift-card-page.png', fullPage: true });
+  console.log('umesh test')
+  await page.goto('https://new.aldi.us/');
+  const xx = await page.getByRole('button', { name: 'Create Account' }).click()
+console.log('xx',xx)
 });
