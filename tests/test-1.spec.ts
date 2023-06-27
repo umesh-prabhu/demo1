03a9191ff5855c371448cfefed4c725d10b713e2
umesh <umesh.prabhu@aldi.us>
2023-06-26 22:52:01 -0500

    ss

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -5,6 +5,8 @@ test('test', async ({ page }) => {
     await page.goto('https://playwright.dev/')
    page.waitForLoadState('domcontentloaded')
   await page.screenshot({ path: 'gift-card-page.png', fullPage: true });
+  await expect(page).toHaveScreenshot()
+  // await expect(page).toHaveScreenshot('gift-card-page.png')
   console.log('umesh test')
  /*  await page.goto('https://new.aldi.us/');
   let xx =  await page.getByRole('link', { name: 'Account' })
