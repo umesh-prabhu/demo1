5b31fc05f1167c778ee947357f93cccf206b9a1f
umesh <umesh.prabhu@aldi.us>
2023-07-03 22:05:48 -0500

    kkk

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,6 +1,15 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
-
+test.beforeAll(async ({ page }) => {
+  console.log('Before tests');
+  await page.goto('https://new.aldi.us/');
+  await page.getByRole('link', { name: 'Account' }).click();
+  await page.getByPlaceholder('Your email address').click();
+  await page.getByPlaceholder('Your email address').fill('someemail@homail.com');
+  await page.getByPlaceholder('Your email address').press('Tab');
+  await page.getByPlaceholder('Your password').fill('Aci12345$');
+  await page.getByRole('button', { name: 'Log In' }).click();
+});
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
