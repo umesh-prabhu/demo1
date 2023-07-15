d65f674bd365a7da762996297ffd45fb32db39d0
umesh <umesh.prabhu@aldi.us>
2023-07-14 23:18:44 -0500

    single test file in yml

--- tests/simple-test.spec.js
+++ tests/simple-test.spec.js
@@ -4,3 +4,5 @@ test('simple hover menu', async ({ page }) => {
   await page.goto('https://new.aldi.us/');
   await page.getByRole('button', { name: 'Recipes' }).hover()
 });
+
+// npx playwright test tests/simple-test.spec.js
